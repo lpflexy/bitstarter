f57801ae5c58b714ecae9937fcb18363107c72d7
lpflexy <lpflexy@gmail.com>
2013-07-14 06:08:52 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var buf=fs.readFileSync('index.html');
+var buf=new Buffer(fs.readFileSync('index.html'));
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
